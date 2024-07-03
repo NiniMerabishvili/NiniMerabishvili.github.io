acd1862426763e5b507b29a4c48e10036601a90a
NiniMerabishvili <22100572@ibsu.edu.ge>
2024-07-03 20:41:29 +0400

    landing-page-design

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: {
+    enabled: true,
+    content: [
+      './src/**/*.html',
+      './src/**/*.ts',
+    ]
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
